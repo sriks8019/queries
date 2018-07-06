76f8366612ffee72480ade08fe2c3ab7a9f219be
Srikanth Ramanam <srikanth.ramanam@hotmail.com>
2018-07-06 01:34:07 -0700

    feedback changes

--- queries_for_dotcom.sql
+++ queries_for_dotcom.sql
@@ -11,32 +11,24 @@ FY18_first_dotcom_customers AS
 SELECT ugc_id, mkt_veh
 FROM
 (SELECT     os.ugc_id, os.visit_date, os.mkt_veh, RANK() OVER( PARTITION BY oc.ugc_id  ORDER BY os.grp_order_nbr) as purchase_number
-FROM gcia_dotcom.omnichannel_sol_v3  os LEFT SEMI JOIN FY18_first_dotcom_customers fpc
+FROM (SELECT * FROM gcia_dotcom.omnichannel_sol_v3  WHERE channel='DOTCOM' AND visit_date BETWEEN   '01-02-2017' AND '31-01-2018' )os
+LEFT SEMI JOIN FY18_first_dotcom_customers fpc
 ON os.ugc_id=fpc.ugc_id
 AND os.visit_date=fpc.fp_dt_dotcom) T
 WHERE T. purchase_number=1
 
 --Q2. 2.	Identify Avg basket size, channel, platform split for all customers in FY18
 -- Query for dotcom customers
-SELECT ugc_id, channel, platform, AVG(basket_size) as average_basket_size
+SELECT ugc_id, service_id,, platform, AVG(basket_size) as average_basket_size
 FROM
-(SELECT ugc_id, channel, platform, grp_order_nbr,   COUNT(qty)  as basket_size -- can use SUM instead of count, if basket size is num of items * qty
+(SELECT ugc_id, service_id, platform, grp_order_nbr,   SUM(qty)  as basket_size -- can use SUM instead of count, if basket size is num of items * qty
 FROM gcia_dotcom.omnichannel_sol_v3
 WHERE fiscal_year_nbr=2018 -- AND visit_date BETWEEN   '01-02-2017' AND '31-01-2018'
 AND channel='DOTCOM'
-GROUP BY ugc_id, channel, platform, grp_order_nbr) T
-GROUP BY ugc_id, channel, platform
+AND service_id NOT IN (19,20)
+GROUP BY ugc_id,service_id, platform, grp_order_nbr) T
+GROUP BY ugc_id, service_id, platform
 
---Query for dotcom and OG
-
-SELECT ugc_id, channel, platform, AVG(basket_size) as average_basket_size
-FROM
-(SELECT ugc_id, channel, platform, grp_order_nbr,   COUNT(qty)  as basket_size -- can use SUM instead of count, if basket size is num of items * qty
-FROM gcia_dotcom.omnichannel_sol_v3
-WHERE fiscal_year_nbr=2018 -- AND visit_date BETWEEN   '01-02-2017' AND '31-01-2018'
-AND channel IN ('DOTCOM', 'OG')
-GROUP BY ugc_id, channel, platform, grp_order_nbr) T
-GROUP BY ugc_id, channel, platform
 
 --Q3. 3.	Calculate churn rate (inactivity of 1 year) for all customers 
 --a.	Identify the reason of higher churn rate
@@ -60,11 +52,17 @@ inactive_dotcom_customers AS
 SELECT ugc_id 
 FROM gcia_dotcom.omnichannel_base_partition_dotcom   -- or  gcia_dotcom.omnichannel_fp_dt_base_final_v3_with_flags   but no filter on partition
 WHERE DATEDIFF(CURRENT_DATE, lp_dt_dotcom) >=365
-)
-
-SELECT oc.ugc_id, COUNT(distinct oc.grp_order_nbr) OVER ( PARTITION BY oc.ugc_id ORDER BY oc.visit_date  ) as txn_num, COUNT(oc.qty)   OVER(PARTITION BY oc.ugc_id , oc.grp_order_nbr ) as basket_size
-FROM gcia_dotcom.omnichannel_sol_v3 oc LEFT SEMI JOIN inactive_dotcom_customers idc
+),
+inactive_dotcom_customer_purchases
+(SELECT oc.ugc_id, oc.grp_order_nbr, SUM(oc.qty) as basket_size
+FROM (SELECT * FROM gcia_dotcom.omnichannel_sol_v3  WHERE channel='DOTCOM' AND visit_date BETWEEN   '01-02-2017' AND '31-01-2018' )oc LEFT SEMI JOIN inactive_dotcom_customers idc
  ON oc.ugc_id=idc.ugc.id
+GROUP BY oc.ugc_d, oc.grp_order_nbr)
 
- 
+SELECT num_orders, COUNT(ugc_id) AS num_of_customers
+(SELECT ugc_id, COUNT(grp_order_number) as num_orders
+FROM inactive_dotcom_customer_purchases
+GROUP BY ugc_id
+)
+ GROUP BY num_orders
  
